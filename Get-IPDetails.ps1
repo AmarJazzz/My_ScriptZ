3771dc843ce067f468d1d71e5065b93009c882d2
Amarnath Mahato <amarnath.m@corp.endurance.com>
2017-09-30 17:54:23 +0530

    Get-IPdetails.ps1

---
+++ Get-IPDetails.ps1
@@ -0,0 +1,78 @@
+﻿<#
+
+.NAME
+    Get-IPDetails
+    
+.SYNOPSIS
+   This script fetches the local ip, public ip and ISP details.
+
+.SYNTAX
+    Get-IPDetails [-Value] <string[]>
+
+.EXAMPLE
+   To Fetch the complete information ( localip, publicip and ISP Details )
+   Get-IPDetails
+
+.EXAMPLE
+   To Fetch your Local IP details
+   Get-IPDetails -Value LocalIP
+
+.EXAMPLE
+   To Fetch your Public IP details
+   Get-IPDetails -Value PublicIP
+
+.EXAMPLE
+   To Fetch your ISP details
+   Get-IPDetails -Value ISP
+
+#>
+
+Try
+{
+Function Get-IPDetails($Value)
+{
+    $Banner = "`n=======================================`n"
+    Function LocalIP
+    {
+         $LocalIP = Get-WmiObject Win32_NetworkAdapterConfiguration|Where {$_.Ipaddress.length -gt 1}
+         $LocalIP=$LocalIP.ipaddress[0];
+         write-host "$Banner Your Local IP is : $LocalIP $Banner" -ForegroundColor Green
+    }
+    
+    Function PublicIP
+    {
+         $PublicIP = Invoke-RestMethod -Uri "https://api.ipify.org"
+         if($PublicIP)
+         {
+            write-host "$Banner Your Public IP is : $PublicIP $Banner" -ForegroundColor Green
+         }
+         else
+         {
+            write-host "Unable to fetch Public IP details" -ForegroundColor Red
+         }       
+    }
+
+   Function ISP-Details
+   {
+          $PublicIP = Invoke-RestMethod -Uri "https://api.ipify.org"
+          $ISP_Details = Invoke-RestMethod -Uri http://ip-api.com/json/$PublicIP
+          write-host "$Banner Below is your ISP Details : $Banner" -ForegroundColor Green
+          write-host ($ISP_Details | Format-List | Out-String ) -ForegroundColor Cyan
+   }
+     
+    Switch ($Value)
+    {
+       LocalIP  { LocalIP }      
+       PublicIP { PublicIP }
+       ISP { ISP-Details }
+       default{ LocalIP ; PublicIP ; ISP-Details}
+    }
+}
+}
+Catch
+{
+    $Exception = $_.Exception
+    $Error_line = $_.InvocationInfo.ScriptLineNumber
+    $Exception_msg = $Exception.Message
+    Write-Host "Error: Got Exception. Full details : $Exception ; Error Message : $Exception_msg ; Error Line number : $Error_line"
+}
