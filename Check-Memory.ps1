3ca449b6098974ab0e8b3cb65f3984b361dc3b7b
Amarnath Mahato <amarnath.m@corp.endurance.com>
2018-03-21 19:46:21 +0530

    Added get-bllip script

--- Check-Memory.ps1
+++ Check-Memory.ps1
@@ -1,6 +1,6 @@
 $Log_File = "C:\Scripts\CheckMemory.log"
 $DateTime = $([datetime]::Now).tostring()
-if(!(Test-Path $Log_File)){New-Item -Path $Log_File -ItemType File | Out-Null}
+if(!(Test-Path $Log_File)){New-Item -Path $Log_File -ItemType File -Force | Out-Null}
 
 $CPU = $proc = (Get-WmiObject -class win32_processor -EA SilentlyContinue | Measure-Object -property LoadPercentage -Average | Select-Object Average | % {$_.Average / 100}).ToString("P")
 $Mem = Get-WmiObject win32_OperatingSystem -EA SilentlyContinue 
---
+++ Get-BLLip.ps1
@@ -0,0 +1,14 @@
+﻿Function Get-BLLIP
+{
+[cmdletbinding()]
+ param(
+    [parameter(Mandatory=$true)]$IP
+ )
+
+$BLL_IPQuery = Invoke-RestMethod "http://bll-1.webhostbox.net:8003/ippool/ipaddress_details.jsp?key=qwedsa&ip=$IP&submit=Submit"
+$Result = $BLL_IPQuery  | findstr "<td>"
+$Result = ($Result[3].trim()) -replace "<td>|</td>",""
+
+Write-Host "IP : $IP`nServerName : $Result" -fore Green
+}
+
