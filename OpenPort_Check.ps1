2585946cc0416c6f96a9b3b6f5abcedd56616eb5
Amarnath Mahato <amarjazzz24@gmail.com>
2022-05-23 23:11:00 +0530

    Find Open Ports on a Server

---
+++ OpenPort_Check.ps1
@@ -0,0 +1,32 @@
+﻿
+$ComputerName = "192.168.43.176"
+$Ports = 1..1000
+
+foreach ($Computer in $ComputerName) 
+{    
+    foreach ($Port in $Ports) 
+    {
+        $Socket = New-Object Net.Sockets.TcpClient
+        $ErrorActionPreference = 'SilentlyContinue'
+        $Socket.Connect($Computer, $Port)
+        $ErrorActionPreference = 'Continue'
+        
+        if ($Socket.Connected) 
+        {
+            write-host "${Computer}: Port $Port is open" -ForegroundColor Green
+            $Socket.Close()
+        }
+        else 
+        {
+            write-host "${Computer}: Port $Port is closed or filtered" -ForegroundColor Red
+        }
+    
+        $Socket.Dispose()
+        $Socket = $null       
+    }  
+}
+
+trap 
+{
+    Write-Host "`nScript Path : $($_.InvocationInfo.ScriptName)`nLine Number : $($_.InvocationInfo.ScriptLineNumber)`nDetail : $(($_.InvocationInfo.Line).trim())`n"
+}
